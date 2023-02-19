aa01714b52afcd3be1e7227398a647ca99876ba7
Spokhim <spokhim@hotmail.com>
2023-02-20 01:26:23 +1100

    Train on all features

--- Train_AutoML.py
+++ Train_AutoML.py
@@ -21,13 +21,15 @@ import pickle
 # Import the Data
 df = pd.read_pickle("Patient_1110.pkl")
 # Specify Columns to Use as Input (in addition to the time-stamp) noting that we'll take all of the catch22 features.
-cols = ["acc_mag","acc_theta","acc_phi",]
+cols = ["acc_mag","acc_theta","acc_phi","bvp","eda","hr"]
+# Specify Columns to use as Input which do not get transformed to feature space
+cols_no_transform = ["time_of_day", "temp"]
 
 ##################################################################################################################
 
 feature_names = np.loadtxt("Catch22_Featurenames",dtype=str)
 col_names = [i + "_" + j for i in cols for j in feature_names]
-col_names = col_names + ['time_of_day'] # Add the quality measurements later.
+col_names = cols_no_transform + col_names # Add the quality measurements later.
 
 # Get time-of-day from timestamp
 df['time_of_day'] = df['utc_timestamp'][0].hour + df['utc_timestamp'][0].minute/60 
